414abbad697e8110313306cce286a9620c327727
kienghub <2kpltteamdev@gmail.com>
2020-10-07 22:22:06 +0700

    update script

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,24 +14,23 @@ function print() {
 function _back(){
   window.history.go(-1);
 }
+
 // delete function
 function _deteteMenu(e){
   Notiflix.Confirm.Show( 'ສະຖານະ', 'ທ່ານຕ້ອງການລຶບຂໍ້ມູນນີ້ແທ້ ຫຼື ບໍ່ ?', 'ຕົກລົງ', 'ຍົກເລີກ',
   function(){
-    window.location='menu.php?del='+e;
+    window.location='menu.php?del=' + e;
   },
   function(){
-  } );
+  });
 }
 
 function _deteteSubMenu(e,ev){
   Notiflix.Confirm.Show( 'ສະຖານະ', 'ທ່ານຕ້ອງການລຶບຂໍ້ມູນນີ້ແທ້ ຫຼື ບໍ່ ?', 'ຕົກລົງ', 'ຍົກເລີກ',
   function(){
-    window.location='sub_menu.php?del='+e+'&page='+ev;
+    window.location='sub_menu.php?del='+ e +'&page='+ ev;
   });
-})
-
-
+}
 
 function _deteteRank(e){
   Notiflix.Confirm.Show( 'ສະຖານະ', 'ທ່ານຕ້ອງການລຶບຂໍ້ມູນນີ້ແທ້ ຫຼື ບໍ່ ?', 'ຕົກລົງ', 'ຍົກເລີກ',
@@ -39,7 +38,7 @@ function _deteteRank(e){
     window.location='add_rank.php?del=' + e;
   },
   function(){
-  } );
+  });
 }
 
 function _deteteStaff(e){
@@ -48,7 +47,6 @@ function _deteteStaff(e){
     window.location='staff-list.php?del='+e;
   });
 }
-
 
 function _deteteGroup(e){
   Notiflix.Confirm.Show( 'ສະຖານະ', 'ທ່ານຕ້ອງການລຶບຂໍ້ມູນນີ້ແທ້ ຫຼື ບໍ່ ?', 'ຕົກລົງ', 'ຍົກເລີກ',
